428429d56eb17a0b3e5ffecc07674ff3d714d013
oinochoe <oinochoe@naver.com>
2020-02-07 22:33:18 +0900

    discord 멤버 추가 기능 발동

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -1,13 +1,20 @@
-// discord를 노드 모듈에서 불러온다.
-const Discord = require('discord.js');
+// Extract the required classes from the discord.js module
+const { Client, Attachment } = require('discord.js');
 // config 세팅
 const { prefix, token } = require('./config.json');
 // 생성자로 client를 생성한다.
-const client = new Discord.Client();
+const client = new Client();
 
 // 클라이언트가 레디되면 console.에 찍어준다.
 client.once('ready', () => {
   console.log('Ready!');
+});
+
+// 멤버가 추가되면 발동한다.
+client.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.find(ch => ch.name === 'member-log');
+  if (!channel) return;
+  channel.send(`Welcome to the server, ${member}`);
 });
 
 // 클라이언트가 메세지를 하면 발동한다.
@@ -15,19 +22,24 @@ client.on('message', message => {
   // message not srtart with '!' prefix .. then return
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  // args
+  // args -> message 콘텐트를 slice
   const args = message.content.slice(prefix.length).split(/ +/);
   const command = args.shift().toLowerCase();
 
-  if (message.content.startsWith(`${prefix}자기야 잘자`)) {
-    message.channel.send('윽 토하겠네.');
-  } else if (message.content.startsWith(`${prefix}beep`)) {
-    message.channel.send('Boop.');
-  } else if (message.content === `${prefix}server`) {
+  if (command === 'rip') {
+    const attachment = new Attachment('https://i.imgur.com/w3duR07.png');
+    message.channel.send(`${message.author},`, attachment);
+  } else if (command === 'ping') {
+    message.reply('Pong! `' + Math.floor(client.ping) + ' ms`');
+  } else if (command.startsWith('섀도우아레나 장비목록')) {
+    message.channel.send('수리검, 나쵸, 양파링');
+  } else if (command.startsWith('섀도우아레나 캐릭터정보')) {
+    message.channel.send('수리검을 날린다 멋있는 캐릭터지 후후 안녕하세요.');
+  } else if (command === 'server') {
     message.channel.send(
       `Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}\nServer creator: ${message.guild.createdAt}\nServer Region: ${message.guild.region}`,
     );
-  } else if (message.content === `${prefix}user-info`) {
+  } else if (command === 'user-info') {
     message.channel.send(
       `Your username: ${message.author.username}\nYour ID: ${message.author.id}`,
     );
@@ -59,9 +71,20 @@ client.on('message', message => {
       return `${user.username}'s avatar: <${user.displayAvatarURL}>`;
     });
 
-    // send the entire array of strings as a message
-    // by default, discord.js will `.join()` the array with `\n`
+    // 재생 test.mp3
     message.channel.send(avatarList);
+  } else if (command === '재생') {
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel
+        .join()
+        .then(connection => {
+          message.reply('재생!');
+          connection.playFile('test.mp3');
+        })
+        .catch(console.log);
+    } else {
+      message.reply('보이스채널에 없네?');
+    }
   }
 });
 
